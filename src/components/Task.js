c3ddce212c649e460e05c2cf0c945fbe0ea50897
Valerie Valentin <valerie.valentine93@gmail.com>
2023-06-13 11:49:41 -0700

    Changed task prop from {} to props

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const buttonClass = isComplete ? 'tasks__item__toggle--completed' : '';
+const Task = (props) => {
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
   return (
     <li className="tasks__item">
-      <button className={`tasks__item__toggle ${buttonClass}`}>{title}</button>
+      <button className={`tasks__item__toggle ${buttonClass}`}>{props.title}</button>
       <button className="tasks__item__remove button">x</button>
     </li>
   );
