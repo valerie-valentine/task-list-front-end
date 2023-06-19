6cb8b2de366e307839254b5157bbc4050ecf7a28
SF <Sabriaford23@gmail.com>
2023-06-18 21:00:22 -0700

    toggle complete feature update wave 2

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,11 +4,24 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = (props) => {
+
+  const onClickButton = () => {
+    const updatedTask = {
+        id: props.id,
+        titleData: props.title,
+        isCompleteData: !props.isComplete
+    };
+
+    // Invoke the function passed in through the prop named "onUpdate"
+    // This function is referenced by the name "updateStudentData" in App
+    props.onUpdate(updatedTask);
+};
   const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
   return (
     <li className="tasks__item">
       <button className={`tasks__item__toggle ${buttonClass}`}>{props.title}</button>
       <button className="tasks__item__remove button">x</button>
+      <button onClick={onClickButton}>Toggle if {props.isComplete} is completed</button>
     </li>
   );
 };
@@ -17,6 +30,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  onUpdate: PropTypes.func.isRequired
 };
 
 export default Task;
